945947e50c4a44e296fbd6e871682df85c777aeb
Adam Wright <adamw@archgrove.co.uk>
2011-03-29 21:04:16 +0100

    Project builds and references RestKit

---
+++ CIAPI/CIAPI/CIClient.h
@@ -0,0 +1,16 @@
+//
+//  CIClient.h
+//  CIAPI
+//
+//  Created by Adam Wright on 29/03/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface CIClient : NSObject {
+    
+}
+
+@end
---
+++ CIAPI/CIAPI/CIClient.m
@@ -0,0 +1,14 @@
+//
+//  CIClient.m
+//  CIAPI
+//
+//  Created by Adam Wright on 29/03/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "CIClient.h"
+
+
+@implementation CIClient
+
+@end
