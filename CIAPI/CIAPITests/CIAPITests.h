3d570003c3b7fbf01e2573ac8275b54fc51d19b6
Adam Wright <adamw@archgrove.co.uk>
2011-03-29 20:59:48 +0100

    Added stub project for CIAPI

---
+++ CIAPI/CIAPITests/CIAPITests.h
@@ -0,0 +1,17 @@
+//
+//  CIAPITests.h
+//  CIAPITests
+//
+//  Created by Adam Wright on 29/03/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface CIAPITests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ CIAPI/CIAPITests/CIAPITests.m
@@ -0,0 +1,33 @@
+//
+//  CIAPITests.m
+//  CIAPITests
+//
+//  Created by Adam Wright on 29/03/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "CIAPITests.h"
+
+
+@implementation CIAPITests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CIAPITests");
+}
+
+@end
