9c84543fe166f5329fa34244b1df8025cb99c629
Adam Wright <adamw@archgrove.co.uk>
2011-04-05 16:45:32 +0100

    Rather ugly script to generate DTO objects from schema

---
+++ DTOGeneration/synthesise.rb
@@ -0,0 +1,152 @@
+#!/usr/bin/ruby
+
+# This is a very hacky script to turn the DTO schema file into 
+# Objective C DTO wrappers
+
+# We use the JSON gem to parse the DTO schema
+require 'rubygems'
+require 'json'
+
+def dtoTypeToNSType(dtoHash)
+  objectMap = { "string" => "NSString",
+                "integer" => "int",
+                "boolean" => "BOOL", 
+                "number" => "NSNumber",
+                "object" => "NSObject",
+                "array" => "NSMutableArray" };
+
+  if (dtoHash.has_key?("extends"))
+    return dtoHash["extends"]["$ref"][2..-1]; 
+  elsif (dtoHash.has_key?("$ref"))
+    return dtoHash["$ref"][2..-1];
+  elsif (objectMap.has_key?(dtoHash["type"]))
+    return objectMap[dtoHash["type"]];
+  end
+
+  raise "Unknown DTO type %s!" % dtoType
+end
+
+def isObject(item)
+  return !["int", "BOOL"].include?(item);
+end
+
+def APIifyName(text)
+  if text == nil
+    return "";
+  end
+
+  if (text.end_with?("DTO"))
+    return "CIAPI" + text[0..-4]
+  else
+    return "CIAPI" + text;
+  end
+end
+
+def createDTOHeader(directory, dtoHash)
+  File.open("%s/%s.h" % [directory, APIifyName(dtoHash["id"])], "w") do |f|
+    f.write "// Generated on %s\n" % DateTime.now
+    f.write "#import <Foundation/Foundation.h>\n\n";
+
+    extends = dtoTypeToNSType(dtoHash);
+
+    f.write "//" + dtoHash["description"] + "\n";
+    f.write "@interface %s : %s \n" % [APIifyName(dtoHash["id"]), extends];
+    f.write "{\n";
+
+    if (dtoHash.has_key?("properties"))
+      dtoHash["properties"].each do |key, value|
+        if (isObject(dtoTypeToNSType(value)))
+          formatString = "  %s *%s;\n";
+        else
+          formatString = "  %s %s;\n";
+        end
+
+        f.write formatString % [APIifyName(dtoTypeToNSType(value)), key]; 
+      end
+    end
+
+    f.write "}\n\n";
+    
+    if (dtoHash.has_key?("properties"))
+      dtoHash["properties"].each do |key, value|
+        f.write "// %s\n" % value["description"]; 
+        if dtoHash.has_value?("demoValue")
+          f.write "// Example: %s\n" % dtoHash["demoValue"];
+        end
+
+        if (isObject(dtoTypeToNSType(value)))
+          formatString = "@property (retain) %s *%s;\n\n";
+        else
+          formatString = "@property %s %s;\n\n";
+        end
+
+        f.write formatString % [APIifyName(dtoTypeToNSType(value)), key]; 
+      end
+    end
+
+    f.write "@end\n\n";
+  end
+end
+
+def createDTOImpl(directory, dtoHash)
+  File.open("%s/%s.m" % [directory, APIifyName(dtoHash["id"])], "w") do |f|
+    f.write "#import \"%s.h\"\n\n" % APIifyName(dtoHash["id"]);
+
+    if (dtoHash.has_key?("properties"))
+      dtoHash["properties"].each do |key, value|
+        if (value.has_key?("$ref"))
+          f.write "#import \"%s.h\"\n" % APIifyName(value["$ref"][2..-1]);
+        end
+      end
+    end
+
+    f.write "\n";
+
+    if (dtoHash.has_key?("properties"))
+      dtoHash["properties"].each do |key, value|
+        f.write "@synthesize %s;\n" % key;
+      end
+    end
+
+    f.write "\n@implementation %s\n" % APIifyName(dtoHash["id"]);
+    f.write "{\n";
+    f.write "}";
+  end
+end
+
+if (ARGV.count != 2)
+  puts "Usage: synthesize.rb INPUT_SCHEMA OUTPUT_DIR"
+  exit
+end
+
+if (!File.exist?(ARGV[0]))
+  puts "Input schema does not exist";
+  exit;
+end
+
+if (!File.directory?(ARGV[1]))
+  puts "Output directory does not exist";
+  exit;
+end
+
+jsonSource = "";
+begin
+  File.open(ARGV[0], "r").each_line do |line|
+    jsonSource += line;
+  end
+  json = JSON.parse(jsonSource);
+rescue
+  puts "Schema JSON was ill-formed";
+  exit
+end
+
+File.open("%s/CIAPIDataTypeObjects.h" % ARGV[1], "w") do |f|
+  f.write "// Generated on %s\n" % DateTime.now
+  json.each do |key, value|
+    if (value != nil)
+      f.write("#import \"%s.h\"\n" % APIifyName(value["id"])); 
+      createDTOHeader(ARGV[1], value);
+      createDTOImpl(ARGV[1], value);
+    end
+  end
+end
