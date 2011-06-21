28e93704ae303665eaf14df5159b840101540a85
Adam Wright <adamw@archgrove.co.uk>
2011-06-21 21:38:02 +0100

    Documentation for test control parameters

--- CIAPI/CIAPITests/TestControl.h
+++ CIAPI/CIAPITests/TestControl.h
@@ -3,7 +3,10 @@
 //  CIAPI
 //
 //  Created by Adam Wright on 24/05/2011.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
 
-#define TEST_REQUESTS 1
+// This will run tests that make requests of the CityIndex test servers, rather than local code units
+// Assuming the network is alive, all tests should pass. However, this can be slow
+// Set to 0 for testing units only, rather than system integration
+
+#define TEST_REQUESTS 0
